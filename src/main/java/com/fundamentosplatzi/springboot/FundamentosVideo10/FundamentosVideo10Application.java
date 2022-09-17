956070c9db49552fe50b452aeebf0e3ed42ea3a7
andres-1891 <andres.cod.1891@gmail.com>
2022-09-16 21:50:46 -0500

    primer commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/FundamentosVideo10/FundamentosVideo10Application.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.FundamentosVideo10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosVideo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosVideo10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springboot/FundamentosVideo10/FundamentosVideo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.FundamentosVideo10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosVideo10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
